68a421cce61524a58291a4ee5e28cba1540f3ced
MKuzera <mateusz.qzera@gmail.com>
2023-03-10 08:43:58 +0100

    2nd Commit: Added: Finished Book class

--- src/Book.java
+++ src/Book.java
@@ -1,8 +1,56 @@
+
+
 public class Book {
+
     String NameOfBook;
     String Author;
     int ID;
-    int RokWydania;
-    int Strony;
+    int PublicationDate;
+    int Pages;
+    static int count = 0;
+    public String getNameOfBook() {
+        return NameOfBook;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getPublicationDate() {
+        return PublicationDate;
+    }
+
+    public int getPages() {
+        return Pages;
+    }
+
+
+    public Book(String nameOfBook, String author, int ID, int rokWydania, int pages) {
+        NameOfBook = nameOfBook;
+        Author = author;
+        this.ID = ID;
+        PublicationDate = rokWydania;
+        Pages = pages;
+        count+=1;
+    }
+
+    public Book(){
+        NameOfBook = "EMPTY";
+        Author = "EMPTY";
+        this.ID = -1;
+        PublicationDate = -1;
+        Pages = -1;
+        count+=1;
+    }
+
+    public void DisplayBook(){
+        System.out.println(this.Author + " " + this.NameOfBook + " ID: " + this.ID +
+                " number of pages" + this.Pages + " publication date:"+ this.PublicationDate);
+    }
+
 
 }
--- src/Library.java
+++ src/Library.java
@@ -1,8 +1,6 @@
 public class Library {
 
     public static void main(String[] args) {
-        for (int i = 0; i<10; i++){
-            Book book;
-        }
+
     }
 }
